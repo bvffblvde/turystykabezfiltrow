8c5313054fb7efdec1bb1ef662c618e48e832a30
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-04-18 12:57:12 +0200

    update server

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ app.get('*', async (request, response)=> {
     }else{
         if(Object.keys(searchedPost).length>0 ){
 
-            sendHTMLFileWithMetadata( '404', '404', '/static/media/main-about-page.*.png',response);
+            // sendHTMLFileWithMetadata( '404', '404', '/static/media/main-about-page.*.png',response);
 
             response.redirect(`/artykuly${urlRequest}`);
 
